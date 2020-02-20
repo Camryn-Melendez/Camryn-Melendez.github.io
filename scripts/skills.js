a0a9ad873d7304577c28f35c6bff98b0d5a3062d
Camryn Melendez <melendez.c@husky.neu.edu>
2020-02-20 17:03:12 -0500

    added js to skills

--- scripts/skills.js
+++ scripts/skills.js
@@ -1,11 +1,11 @@
-//Scroll the window to the appropriate section when a link in the sidebar is pressed
-var languages = $('languages-button');
-$('window').animate({scrollTop: languages.position().top});
-
-var vcs = $('vcs-button');
-$('window').animate({scrollTop: vcs.position().top});
-
-document.getElementById('languages-button').onclick = function() {
-    var div = $('accordionExample');
-    $('window').animate({scrollTop: div.position().top});
+//Scrolls to a specific position based on the div ID given.
+function scrollToSection( ID ) {
+    var posToScrollTo = $( ID ).position();
+    window.scrollTo( 0, posToScrollTo.top );
 }
+
+document.getElementById( 'languages-button' ).onclick = function() { scrollToSection( '#languages-section' ) } ;
+
+document.getElementById( 'vcs-button' ).onclick = function() { scrollToSection( '#vcs-section' ) } ;
+
+document.getElementById( 'coursework-button' ).onclick = function() { scrollToSection( '#coursework-section' ) } ;
