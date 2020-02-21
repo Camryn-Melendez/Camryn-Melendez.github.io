1aeafe234e6cda79298f1d45a7d7e21e73501b84
Camryn Melendez <melendez.c@husky.neu.edu>
2020-02-21 12:49:35 -0500

    got skills sidebar to stick!

--- scripts/skills.js
+++ scripts/skills.js
@@ -1,7 +1,7 @@
 //Scrolls to a specific position based on the div ID given.
 function scrollToSection( ID ) {
     var posToScrollTo = $( ID ).position();
-    window.scrollTo( 0, posToScrollTo.top );
+    window.scrollTo( 0, posToScrollTo.top + 150 );
 }
 
 document.getElementById( 'languages-button' ).onclick = function() { scrollToSection( '#languages-section' ) } ;
@@ -9,3 +9,5 @@ document.getElementById( 'languages-button' ).onclick = function() { scrollToSec
 document.getElementById( 'vcs-button' ).onclick = function() { scrollToSection( '#vcs-section' ) } ;
 
 document.getElementById( 'coursework-button' ).onclick = function() { scrollToSection( '#coursework-section' ) } ;
+
+
