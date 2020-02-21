827689a8fce63fcb10a444ffbc51f4e28153b99b
Camryn Melendez <melendez.c@husky.neu.edu>
2020-02-21 15:27:30 -0500

    updated skills

--- scripts/skills.js
+++ scripts/skills.js
@@ -4,10 +4,9 @@ function scrollToSection( ID ) {
     window.scrollTo( 0, posToScrollTo.top + 150 );
 }
 
-document.getElementById( 'languages-button' ).onclick = function() { scrollToSection( '#languages-section' ) } ;
-
-document.getElementById( 'vcs-button' ).onclick = function() { scrollToSection( '#vcs-section' ) } ;
-
-document.getElementById( 'coursework-button' ).onclick = function() { scrollToSection( '#coursework-section' ) } ;
-
-
+document.getElementById( 'languages-button' ).onclick = function() { scrollToSection( '#languages-section' ) };
+document.getElementById( 'vcs-button' ).onclick = function() { scrollToSection( '#vcs-section' ) };
+document.getElementById( 'coursework-button' ).onclick = function() { scrollToSection( '#coursework-section' ) };
+document.getElementById( 'libraries-button' ).onclick = function() { scrollToSection( '#libraries-section' ) };
+document.getElementById( 'ide-button' ).onclick = function() { scrollToSection( '#ide-section' ) };
+document.getElementById( 'adobe-button' ).onclick = function() { scrollToSection( '#adobe-section' ) };
